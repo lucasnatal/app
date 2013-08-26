2931c7983d68220dee0d4b712420dea91801c627
Lucas Natal <bonettinatal@gmail.com>
2013-08-26 11:13:46 -0300

    Commit inicial

---
+++ App/src/com/betha/beans/InicioBean.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.betha.beans;
+
+/**
+ * @author Administrador
+ *
+ */
+public class InicioBean {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void enviar(){
+		
+	}
+}
